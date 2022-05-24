4c410bea2421814ae0ad0ffed6f80536fb4ce660
pedro048 <pedro,andrade046@gmail.com>
2022-05-24 12:50:08 -0300

    commit again

---
+++ source/api/query_live_api.py
@@ -0,0 +1,50 @@
+"""
+Creators: Pedro Victor and Beatriz Soares
+Date: 23 May 2022
+Script that POSTS to the API using the requests 
+module and returns both the result of 
+model inference and the status code
+"""
+import requests
+import json
+# import pprint
+
+person = {
+    "Age_band_of_driver": '18-30',
+    "Sex_of_driver": 'Female',
+    "Educational_level": 'High school',
+    "Vehicle_driver_relation": 'Employee',
+    "Driving_experience":  '2-5yr',
+    "Lanes_or_Medians": 'One way',
+    "Types_of_Junction": 'Y Shape',
+    "Road_surface_type": 'Asphalt roads',
+    "Light_conditions": 'Daylight',
+    "Weather_conditions": 'Normal',
+    "Type_of_collision": 'Vehicle with vehicle collision',
+    "Vehicle_movement": 'Going straight',
+    "Pedestrian_movement": 'Not a Pedestrian',
+    "Cause_of_accident": 'No priority to vehicle'
+    }
+
+url = "http://127.0.0.1:8000"
+#url = "https://high-income-app.herokuapp.com"
+response = requests.post(f"{url}/predict",
+                         json=person)
+
+print(f"Request: {url}/predict")
+print(f"Person: \n Age_band_of_driver: {person['Age_band_of_driver']}\n Sex_of_driver: {person['Sex_of_driver']}\n"\
+      f" Educational_level: {person['Educational_level']}\n"\
+      f" Vehicle_driver_relation: {person['Vehicle_driver_relation']}\n"\
+      f" Driving_experience: {person['Driving_experience']}\n"\
+      f" Lanes_or_Medians: {person['Lanes_or_Medians']}\n"\
+      f" Types_of_Junction: {person['Types_of_Junction']}\n"\
+      f" Road_surface_type: {person['Road_surface_type']}\n"\
+      f" Light_conditions: {person['Light_conditions']}\n"\
+      f" Weather_conditions: {person['Weather_conditions']}\n"\
+      f" Type_of_collision: {person['Type_of_collision']}\n"\
+      f" Vehicle_movement: {person['Vehicle_movement']}\n"\
+      f" Pedestrian_movement: {person['Pedestrian_movement']}\n"
+      f" Cause_of_accident: {person['Cause_of_accident']}\n"
+     )
+print(f"Result of model inference: {response.json()}")
+print(f"Status code: {response.status_code}")
---
+++ source/api/test_main.py
@@ -0,0 +1,96 @@
+"""
+Creators: Pedro Victor and Beatriz Soares
+Date: 23 May 2022
+Create API
+"""
+from fastapi.testclient import TestClient
+import os
+import sys
+import pathlib
+from source.api.main import app
+
+# Instantiate the testing client with our app.
+client = TestClient(app)
+
+# a unit test that tests the status code of the root path
+def test_root():
+    r = client.get("/")
+    assert r.status_code == 200
+
+# a unit test that tests the status code and response 
+# for an instance with a low income
+def test_get_Accident_severity_0():
+
+    person = {
+        "Age_band_of_driver": '18-30',
+        "Sex_of_driver": 'Male',
+        "Educational_level": 'Unknown',
+        "Vehicle_driver_relation": 'Unknown',
+        "Driving_experience":  'Unknown',
+        "Lanes_or_Medians": 'Two-way (divided with broken lines road marking)',
+        "Types_of_Junction": 'Y Shape',
+        "Road_surface_type": 'Asphalt roads',
+        "Light_conditions": 'Darkness - lights lit',
+        "Weather_conditions": 'Raining',
+        "Type_of_collision": 'Collision with pedestrians',
+        "Vehicle_movement": 'Going straight',
+        "Pedestrian_movement": 'Not a Pedestrian',
+        "Cause_of_accident": 'Driving under the influence of drugs'
+    }
+
+    r = client.post("/predict", json=person)
+    # print(r.json())
+    assert r.status_code == 200
+    assert r.json() == "Accident_severity: 0"
+
+# a unit test that tests the status code and response 
+# for an instance with a high income
+def test_get_Accident_severity_1():
+
+    person = {
+        "Age_band_of_driver": '18-30',
+        "Sex_of_driver": 'Male',
+        "Educational_level": 'Junior high school',
+        "Vehicle_driver_relation": 'Employee',
+        "Driving_experience":  '1-2yr',
+        "Lanes_or_Medians": 'other',
+        "Types_of_Junction": 'No junction',
+        "Road_surface_type": 'Asphalt roads',
+        "Light_conditions": 'Daylight',
+        "Weather_conditions": 'Normal',
+        "Type_of_collision": 'Collision with roadside objects',
+        "Vehicle_movement": 'Going straight',
+        "Pedestrian_movement": 'Not a Pedestrian',
+        "Cause_of_accident": 'Changing lane to the left'
+    }
+
+    r = client.post("/predict", json=person)
+    print(r.json())
+    assert r.status_code == 200
+    assert r.json() == "Accident_severity: 1"
+    
+# a unit test that tests the status code and response 
+# for an instance with a high income
+def test_get_Accident_severity_2():
+
+    person = {
+        "Age_band_of_driver": '18-30',
+        "Sex_of_driver": 'Male',
+        "Educational_level": 'Above high school',
+        "Vehicle_driver_relation": 'Employee',
+        "Driving_experience":  '1-2yr',
+        "Lanes_or_Medians": 'Unknown',
+        "Types_of_Junction": 'No junction',
+        "Road_surface_type": 'Asphalt roads',
+        "Light_conditions": 'Daylight',
+        "Weather_conditions": 'Normal',
+        "Type_of_collision": 'Collision with roadside-parked vehicles',
+        "Vehicle_movement": 'Going straight',
+        "Pedestrian_movement": 'Not a Pedestrian',
+        "Cause_of_accident": 'Moving Backward'
+    }
+
+    r = client.post("/predict", json=person)
+    print(r.json())
+    assert r.status_code == 200
+    assert r.json() == "Accident_severity: 2"
